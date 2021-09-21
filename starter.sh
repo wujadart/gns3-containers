61c2534751816dc72f17f44acded086e73873b36
wujadart <91125364+wujadart@users.noreply.github.com>
2021-09-21 14:08:54 +0200

    Add files via upload

---
+++ starter.sh
@@ -0,0 +1,18 @@
+# (Re)starting services
+# snmpd hangs after stop, it needs to be killed manually
+/etc/init.d/ssh start >/dev/null 2>/dev/null
+/etc/init.d/rsyslog start >/dev/null 2>/dev/null
+/etc/init.d/nginx start >/dev/null 2>/dev/null
+/etc/init.d/vsftpd start >/dev/null 2>/dev/null
+/etc/init.d/tftpd-hpa start >/dev/null 2>/dev/null
+SNMPD_PID=`cat /run/snmpd.pid`
+/etc/init.d/snmpd stop >/dev/null 2>/dev/null
+kill $SNMPD_PID
+/etc/init.d/snmpd start >/dev/null 2>/dev/null
+/etc/init.d/snmptrapd start >/dev/null 2>/dev/null
+
+clear
+
+# Launching shell
+cd
+exec bash -i
